44e9d67bb5ed8607748adaa34229dd7919d4ba0a
MortadhaGaied <mortadha.gaied@esprit.tn>
2021-10-09 18:30:45 +0100

    CRUD reclamation

--- src/dev/masters/test/Main.java
+++ src/dev/masters/test/Main.java
@@ -5,6 +5,7 @@
  */
 package dev.masters.test;
 
+
 /**
  *
  * @author Mortadha
@@ -15,7 +16,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
